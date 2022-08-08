3179100bbe51d222c534d0c4700e2a64ee691db9
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2022-08-08 16:04:54 +0100

    Add hamburger navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { MdClose } from 'react-icons/md'
+import { FiMenu } from 'react-icons/fi'
 
 function NavItem({ href, text }) {
   const location = useLocation();
@@ -21,11 +23,21 @@ function NavItem({ href, text }) {
 }
 
 function Navigation() {
+  const [navOpen, setNavOpen] = useState(false)
+
+  function handleToggle() {
+    setNavOpen(prev => !prev)
+  }
 
   return (
     <header className="header">
       <nav className="navbar">
-        <ul className="nav__menu">
+        <button className='nav__button' onClick={handleToggle}>{navOpen ?
+          <MdClose style={{ color: "#3e3d3d", width: "30px", height: "30px" }} />
+          :
+          <FiMenu style={{ color: "#000000", width: "30px", height: "30px" }} />}
+        </button>
+        <ul className={`nav__menu ${navOpen ? "nav__menu--open" : "nav__menu--closed"}`}>
           <NavItem href="/" text="Home" />
           <NavItem href="/about" text="About" />
           <NavItem href="/experience" text="Experience" />
