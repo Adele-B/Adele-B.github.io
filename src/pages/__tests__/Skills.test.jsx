337bb06efdc4c9eef7b702d52c889e7039825e9c
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2022-08-12 10:10:29 +0100

    Create skills test

---
+++ src/pages/__tests__/Skills.test.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event'
+import Skills from '../Skills';
+
+const renderPage = () => {
+  return render(
+    <BrowserRouter>
+      <Skills />
+    </BrowserRouter>,
+  );
+};
+
+describe('Skills page', () => {
+  it('should reveal text when button is clicked', async () => {
+    const user = userEvent.setup()
+    
+    renderPage();
+
+    const btn = screen.getByRole('button', {name: 'Show text'})
+
+    expect(screen.queryByText("React.js - My most proficient language")).toHaveClass('skills__text');
+    await user.click(btn)
+    expect(screen.queryByText("React.js - My most proficient language")).toHaveClass('skills__text--clicked')
+
+  })
+})
